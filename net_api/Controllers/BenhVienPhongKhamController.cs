2754e7b758dc5599f4b5312d4f5cc765e6c6b33c
dungdv <dungdv@gmail.com>
2023-07-22 08:30:27 +0700

    phòng khám bệnh việnh

---
+++ net_api/Controllers/BenhVienPhongKhamController.cs
@@ -0,0 +1,105 @@
+﻿using BiboCareServices.Middleware;
+using BiboCareServices.Models;
+using Lib.Utils.Package;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BiboCareServices.Controllers
+{
+    [ApiController]
+    [Route("Bibocare")]
+    public class BenhVienPhongKhamController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetListBenhVienPhongKham")]
+        public async Task<IActionResult> GetListBenhVienPhongKham()
+        {
+            RouteListBenhVienPhongKham res = new RouteListBenhVienPhongKham();
+
+            try
+            {
+                List<BenhVienPhongKham> lsItem = SqlHelper.ExecuteList<BenhVienPhongKham>(EnvMiddleware.GetValue("strConn"), "sp_getlist_phongkhambenhvien");
+                if (lsItem.Count() > 0)
+                {
+
+                    res.status = 200;
+                    res.message = lsItem;
+                }
+                else
+                {
+                    res.status = 400;
+                }
+            }
+            catch (Exception ex)
+            {
+                DataAccess.LogBuild.CreateLogger(JsonConvert.SerializeObject(ex), "GetListBenhVienPhongKham");
+                return BadRequest(ex.Message);
+            }
+            return Ok(res);
+        }
+
+        [HttpGet]
+        [Route("GetListLichKham")]
+        public async Task<IActionResult> GetListLichKham(string UserId)
+        {
+            RouteListLichKham res = new RouteListLichKham();
+
+            try
+            {
+                List<LichKham> lsItem = SqlHelper.ExecuteList<LichKham>(EnvMiddleware.GetValue("strConn"), "sp_getlist_lichkham", UserId);
+                if (lsItem.Count() > 0)
+                {
+
+                    res.status = 200;
+                    res.message = lsItem;
+                }
+                else
+                {
+                    res.status = 400;
+                }
+            }
+            catch (Exception ex)
+            {
+                DataAccess.LogBuild.CreateLogger(JsonConvert.SerializeObject(ex), "GetListLichKham");
+                return BadRequest(ex.Message);
+            }
+            return Ok(res);
+        }
+
+        [HttpPost]
+        [Route("HenLichKham")]
+        public async Task<IActionResult> HenLichKham(LichKham lichKham)
+        {
+            try
+            {
+                lichKham.CreateBy = "test";
+                lichKham.Status = "Chờ xác nhận";
+                int rs = SqlHelper.ExecuteNonQuery(EnvMiddleware.GetValue("strConn"), "sp_insert_lichkham",
+                    lichKham.HoTen,
+                    lichKham.SoDienThoai,
+                    lichKham.BenhVienPhongKhamId,
+                    lichKham.NgayKham,
+                    lichKham.GioKham,
+                    lichKham.NoiDungKham,
+                    lichKham.CreateBy,
+                    lichKham.Status
+                    );
+                if (rs > 0)
+                {
+                    return Ok(new { status = 200, message = "Đặt lịch thành công" });
+                }
+                else
+                {
+                    return BadRequest(new { status = 400, message = "Đặt lịch thất bại! Có lỗi xảy ra" });
+                }
+            }
+            catch (Exception ex)
+            {
+                DataAccess.LogBuild.CreateLogger(JsonConvert.SerializeObject(ex), "HenLichKham");
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ net_api/Models/BenhVienPhongKham.cs
@@ -0,0 +1,33 @@
+﻿namespace BiboCareServices.Models
+{
+    public class BenhVienPhongKham
+    {
+        public long Id { get; set; }
+        public string Ten { get; set; }
+        public string DiaChi { get; set; }
+        public string DienThoai { get; set; }
+    }
+    public class RouteListBenhVienPhongKham
+    {
+        public int status { set; get; }
+        public List<BenhVienPhongKham> message { set; get; }
+    }
+    public class LichKham
+    {
+        public long Id { get; set; }
+        public string HoTen { get; set; }
+        public string SoDienThoai { get; set; }
+        public string Status { get; set; }
+        public long BenhVienPhongKhamId { get; set; }
+        public DateTime NgayKham { get; set; }
+        public string GioKham { get; set; }
+        public string NoiDungKham { get; set; }
+        public string CreateBy { get; set; }
+    }
+
+    public class RouteListLichKham
+    {
+        public int status { set; get; }
+        public List<LichKham> message { set; get; }
+    }
+}
